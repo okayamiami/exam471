47bc3f6ab348f17071b9fa1e4200f8a630c21f29
okayamiami <knz2374471@stu.o-hara.ac.jp>
2024-04-25 15:10:08 +0900

    4/25岡山

---
+++ src/scoremanager/main/TestListAction.java
@@ -0,0 +1,40 @@
+package scoremanager.main;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import dao.ClassNumDao;
+import tool.Action;
+
+public class TestListAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		HttpSession session = req.getSession();// セッションを取得
+		Teacher teacher = (Teacher) session.getAttribute("user");// ログインユーザーを取得
+		ClassNumDao cNumDao = new ClassNumDao();// クラス番号Daoを初期化
+		SubjectDao subDao = new SubjectDao();// 科目Daoを初期化
+		LocalDate todaysDate = LocalDate.now();// LcalDateインスタンスを取得
+		int year = todaysDate.getYear();// 現在の年を取得
+		List<Integer> entYearSet = new ArrayList<>();//入学年度のリストを初期化
+
+
+		List<String> clist = cNumDao.filter(teacher.getSchool());// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
+		List<String> sublist = subDao.filter(teacher.getSchool());// ログインユーザーの学校コードをもとに科目の一覧を取得
+
+		for (int i = year - 10; i < year + 10; i++) {
+			entYearSet.add(i);
+		}// 現在を起点に前後10年をリストに追加
+
+		req.setAttribute("class_num_set", clist);//クラス番号のlistをセット
+		req.setAttribute("subjet_set", sublist);//のlistをセット
+	}
+
+
+}
---
+++ src/scoremanager/main/TestListSubjectExecuteAction.java
@@ -0,0 +1,16 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class TestListSubjectExecuteAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+
+	}
+
+}
