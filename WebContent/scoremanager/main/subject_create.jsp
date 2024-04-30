592d762d5afb577eab2a857baeca189db69129d4
daichi2374506 <knz2374506@stu.o-hara.ac.jp>
2024-04-30 12:35:21 +0900

    4/30根来

---
+++ WebContent/scoremanager/main/subject_create.jsp
@@ -0,0 +1,33 @@
+<%-- 科目登録JSP --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>得点管理システム</title>
+</head>
+<body>
+	<h2>科目情報 新規登録</h2>
+
+	<form action = "StudentCreateExecute.action" method="post">
+
+		<label>科目コード</label>
+		<input type="text" name="subject_cd"
+			placeholder="科目コードを入力してください" maxlength="10" value="${subject_cd}" required />
+		<div>${errors.get("subject_cd")}</div>
+
+		<label>科目名</label>
+		<input type="text" name="subject_name"
+			placeholder="科目名を入力してください" maxlength="10" value="${subject_name}" required />
+		<div>${errors.get("subject_name")}</div>
+
+
+		<input type="submit" value="登録">
+	</form>
+
+	<a href="SubjectList.action">戻る</a>
+
+</body>
+</html>
---
+++ src/scoremanager/main/SubjectCreateExecuteAction.java
@@ -0,0 +1,70 @@
+package scoremanager.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectCreateExecuteAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		//ローカル変数の宣言 1
+		HttpSession session = req.getSession();//セッション
+		SubjectDao sbDao = new SubjectDao();//科目Dao
+		String subject_cd = "";//科目コード
+
+		Subject subject = null;
+
+		String subject_name = "";//科目名
+		Map<String, String> errors = new HashMap<>();// エラーメッセージ
+		Teacher teacher = (Teacher) session.getAttribute("user");// ログインユーザーを取得
+
+		//リクエストパラメータ―の取得 2
+		subject_cd = req.getParameter("subject_cd");//科目コード
+		subject_name = req.getParameter("subject_name");//科目名
+
+		//ビジネスロジック 4
+		//DBへデータ保存 5
+		//条件で手順4~5の内容が分岐
+
+		if (subject == null) {// 科目が未登録だった場合
+		// 科目インスタンスを初期化
+		subject = new Subject();
+		// インスタンスに値をセット
+		subject.setSubject_cd(subject_cd);
+		subject.setName(subject_name);
+
+		// 科目情報を保存
+		sbDao.save(subject);
+
+		} else{//入力された学番がDBに保存されていた場合
+		errors.put("subject_cd", "科目コードが重複しています");
+		}
+
+
+		//レスポンス値をセット 6
+		//JSPへフォワード 7
+
+		//System.out.println("3-2");
+		if(!errors.isEmpty()){
+			// リクエスト属性をセット
+			req.setAttribute("errors", errors);
+			req.setAttribute("subject_cd", subject_cd);
+			req.setAttribute("subject_name", subject_name);
+
+			req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+
+			return;
+		}
+		req.getRequestDispatcher("subject_create_done.jsp").forward(req, res);
+	}
+}
