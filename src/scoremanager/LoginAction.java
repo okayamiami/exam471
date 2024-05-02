66cec039c0f11ae1bd6c6120da3ea56563cb471f
2374501 <2374501@192.168.0.16>
2024-05-02 12:32:01 +0900

    5/02 桶谷

--- src/scoremanager/LoginAction.java
+++ src/scoremanager/LoginAction.java
@@ -2,7 +2,10 @@ package scoremanager;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Teacher;
+import dao.TeacherDAO;
 import tool.Action;
 
 public class LoginAction extends Action{
@@ -11,6 +14,28 @@ public class LoginAction extends Action{
 	public void execute(
 			HttpServletRequest req, HttpServletResponse res
 		) throws Exception {
+
+
+		//セッション取得
+		HttpSession session=req.getSession();
+
+		//ログイン名とパスワードを取得
+		String login=req.getParameter("login");
+		String password=req.getParameter("password");
+
+		//データベースからデータ検索
+		TeacherDAO dao=new TeacherDAO();
+		Teacher teacher=dao.search(login, password);
+
+		//ある場合
+		if (teacher!=null) {
+			session.setAttribute("teacher", teacher);
+			return "login.jsp";
+		}
+		//ない場合
+		return "error.jsp";
+	}
+}
 
 		req.getRequestDispatcher("login.jsp").forward(req, res);
 
